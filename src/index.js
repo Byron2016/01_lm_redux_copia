09acea82b31e9d5ee5ddd84206e51544d162747f
MiPersona <bgva2005@yahoo.com>
2020-05-24 20:48:52 -0500

    0002_Ejemplo Arq. redux funcionando

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-console.log("Hola Mundo");
-console.log("Hola mundo2");
+import { createStore } from './redux';
+
+function reducer(state = {}, action) {
+	switch(action.type) {
+		case 'NUEVA':
+			return action.payload;
+
+		default:
+			return state;
+	}
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+	const state = store.getState();
+			
+	// console.log('subscribe');
+	console.log(state);
+});
+
+
+
+setInterval(() => {
+	store.dispatch({
+		type: 'NUEVA',
+		payload: {
+			value: Math.random(),
+		},
+	});
+}, 1000);
