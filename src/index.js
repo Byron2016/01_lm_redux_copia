637784fe1dbb387d31edf98f48a1db65918fee3d
MiPersona <bgva2005@yahoo.com>
2020-05-24 22:30:58 -0500

    0004_Funcional agrega item quemado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-console.log("h");
+import store from './redux/store';
+
+const itemListDOM = $('#itemList');
+const itemDOM = $('#item');
+
+function actualizarLista(items) {
+	itemListDOM.html('');
+
+	for (const item of items) {
+		const cloneDOM = itemDOM.clone();
+		const chkHabilitadoDOM = cloneDOM.find('input');
+		const btnBorrarDOM = cloneDOM.find('button');
+		const lblNombreDOM = cloneDOM.find('span');
+
+		cloneDOM.removeClass('d-none');
+		itemListDOM.append(cloneDOM);
+
+		lblNombreDOM.html(item.text);
+		if (item.completado) {
+			lblNombreDOM.css('text-decoration', 'line-through');
+		}
+
+		btnBorrarDOM.on('click', () => {
+			store.dispatch({
+				type: 'BORRAR',
+				payload: {
+					id: item.id,
+				},
+			});
+		});
+
+		chkHabilitadoDOM.prop('checked', item.completado);
+		chkHabilitadoDOM.on('click', () => {
+			store.dispatch({
+				type: 'ALTERNAR',
+				payload: {
+					id: item.id,
+				},
+			});
+		})
+
+		
+	}
+}
+
+store.subscribe(() => {
+	const state = store.getState();
+
+	actualizarLista(state);
+});
+
+store.dispatch({
+	type: 'AGREGAR',
+	payload: {
+		text: 'hola mundo',
+	},
+});
